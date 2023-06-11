edbaaec0b5cf576db9cc5fee945e7905293b3faf
David Lopez Felix <dlopezf.19@gmail.com>
2023-06-11 12:59:13 -0500

    fixturing

--- cypress/e2e/countrylayer.cy.js
+++ cypress/e2e/countrylayer.cy.js
@@ -1,15 +1,8 @@
-///<reference types="Cypress"/>
+import accessData from '../fixtures/accessKey.json'
 
 describe('TestSuite', function() {
-  //declaring accessToken 
-  const queryParam = {access_key: "d250e0a80e0e67f18866958c3f56915d"};
-
-  //declaring alphacode plus country associated with
-  const country1 = ["US", "United States of America"];
-  const country2 = ["DE", "Germany"];
-  const country3 = ["GB", "United Kingdom of Great Britain and Northern Ireland"];
-
-  const country4 = "kk" //non-existent
+  //declaring accessToken
+  const queryParam = {access_key: accessData.access_key};
 
   //new country for POST method
   const new_country = {
@@ -19,67 +12,38 @@ describe('TestSuite', function() {
                       }
 
   //Getting US country
-  it('Getting US', function(){
-    cy.request({
-      method:'GET',
-      url: `http://api.countrylayer.com/v2/alpha/${country1[0]}`,
-      qs: queryParam //
+  it('Getting the three countries (US, DE and GB) and non-existent (KK)', function(){
+
+    cy.fixture('countries.json').then((fixtureData) => {
+
+      fixtureData.forEach((country) => {
+        cy.request({
+          method:'GET',
+          url: `http://api.countrylayer.com/v2/alpha/${country.alphacode}`,
+          qs: queryParam,
+          failOnStatusCode: false
+        })
+        .then((response) => {
+          //Validations
+          if (response.status == 200){
+            expect(response.status).eq(country.code)
+            expect(response.body).has.property("name", country.name)
+            expect(response.body).has.property("alpha2Code", country.alphacode)
+          }
+          else{
+            expect(response.status).eq(country.code)
+          }
+
+        })
+        cy.wait(1000) //rate limitation for free plan
+
+      })
+
     })
-    .then((response) => {
-      //Validations
-      expect(response.status).eq(200)
-      expect(response.body).has.property("name", country1[1])
-      expect(response.body).has.property("alpha2Code", country1[0])
-    })
-    cy.wait(1000) //rate limitation for free plan
+
+
   })
 
-  //Getting DE country
-  it('Getting DE', function(){
-    cy.request({
-      method:'GET',
-      url: `http://api.countrylayer.com/v2/alpha/${country2[0]}`,
-      qs: queryParam
-    })
-    .then((response) => {
-      //Validations
-      expect(response.status).eq(200)
-      expect(response.body).has.property("name", country2[1])
-      expect(response.body).has.property("alpha2Code", country2[0])
-    })
-    cy.wait(1000) //rate limitation for free plan
-  })
-
-  //Getting GB country
-  it('Getting GB', function(){
-    cy.request({
-      method:'GET',
-      url: `http://api.countrylayer.com/v2/alpha/${country3[0]}`,
-      qs: queryParam
-    })
-    .then((response) => {
-      //Validations
-      expect(response.status).eq(200)
-      expect(response.body).has.property("name", country3[1])
-      expect(response.body).has.property("alpha2Code", country3[0])
-    })
-    cy.wait(1000) //rate limitation for free plan
-  })
-
-  //Getting non-existent country
-  it('Getting KK (non-existent)', function(){
-    cy.request({
-      method:'GET',
-      url: `http://api.countrylayer.com/v2/alpha/${country4}`,
-      qs: queryParam,
-      failOnStatusCode: false
-    })
-    .then((response) => {
-      //Validations
-      expect(response.status).eq(404)
-      //expect(response.body).has.property("message", "Not found")
-    })
-  })
 
   //This case is skipped because the POST method does not exist
   //and it breaks the test suite.
